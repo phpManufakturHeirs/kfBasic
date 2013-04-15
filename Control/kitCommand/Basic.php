b66c4de10213816e589fa245b7a4cc363bec535c
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-04-15 15:58:07 +0200

    Added class kitCommand/Basic

--- Control/kitCommand/Basic.php
+++ Control/kitCommand/Basic.php
@@ -13,16 +13,17 @@ namespace phpManufaktur\Basic\Control\kitCommand;
 
 use Silex\Application;
 
-class Basic 
+class Basic
 {
-    
+
     protected $app = null;
     private static $cms_parameters = null;
     protected static $cms = null;
     protected static $parameter = null;
     protected static $GET = null;
     protected static $POST = null;
-    
+    private static $message = '';
+
     public function __construct(Application $app, $parameters)
     {
         $this->app = $app;
@@ -30,8 +31,42 @@ class Basic
         Basic::$cms = Basic::$cms_parameters['cms'];
         Basic::$parameter = Basic::$cms_parameters['params'];
         Basic::$GET = Basic::$cms_parameters['GET'];
-        Basic::$POST = Basic::$cms_parameters['POST'];        
+        Basic::$POST = Basic::$cms_parameters['POST'];
+        $app['translator']->setLocale(Basic::$cms['locale']);
     }
-    
-    
+
+    /**
+     * @return the $message
+     */
+    public static function getMessage ()
+    {
+        return Basic::$message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public static function setMessage ($message)
+    {
+        Basic::$message .= $message;
+    }
+
+    /**
+     * Check if a message is active
+     *
+     * @return boolean
+     */
+    public static function isMessage()
+    {
+        return !empty(Basic::$message);
+    }
+
+    /**
+     * Clear the existing message(s)
+     */
+    public static function clearMessage()
+    {
+        Basic::$message = '';
+    }
+
 }
