fae116a141f97b036c686f03217981224ba9da19
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-04-16 13:02:56 +0200

    add function createIFrame() for kitCommand

--- Control/kitCommand/Basic.php
+++ Control/kitCommand/Basic.php
@@ -18,21 +18,28 @@ class Basic
 
     protected $app = null;
     private static $cms_parameters = null;
+    protected static $parameter_string = null;
     protected static $cms = null;
     protected static $parameter = null;
     protected static $GET = null;
     protected static $POST = null;
+    protected static $SESSION = null;
     private static $message = '';
+    protected static $preferred_template = null;
 
     public function __construct(Application $app, $parameters)
     {
         $this->app = $app;
+        Basic::$parameter_string = $parameters;
         Basic::$cms_parameters = json_decode(base64_decode($parameters), true);
         Basic::$cms = Basic::$cms_parameters['cms'];
         Basic::$parameter = Basic::$cms_parameters['params'];
         Basic::$GET = Basic::$cms_parameters['GET'];
         Basic::$POST = Basic::$cms_parameters['POST'];
+        Basic::$SESSION = Basic::$cms_parameters['SESSION'];
         $app['translator']->setLocale(Basic::$cms['locale']);
+        // check for the preferred template
+        Basic::$preferred_template = (isset(Basic::$parameter['template'])) ? Basic::$parameter['template'] : '';
     }
 
     /**
@@ -67,6 +74,21 @@ class Basic
     public static function clearMessage()
     {
         Basic::$message = '';
+    }
+
+    /**
+     * Create a iFrame for embedding a kitCommand within a Content Management System
+     *
+     * @param string $source the content URL of the iFrame
+     */
+    public function createIFrame($source)
+    {
+        return $this->app['twig']->render($this->app['utils']->templateFile('@phpManufaktur/Basic/Template', 'iframe.kitcommand.twig', self::$preferred_template),
+            array(
+            'width' => (isset(self::$parameter['width'])) ? self::$parameter['width'] : '100%',
+            'height' => (isset(self::$parameter['height'])) ? self::$parameter['height'] : '400px',
+            'source' => $source
+            ));
     }
 
 }
