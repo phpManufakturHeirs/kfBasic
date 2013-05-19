5d53e2468eb843f2db3f3f624dc764ba743e2f4e
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-05-19 15:11:04 +0200

    added getHelpPage()

--- Control/kitCommand/Help.php
+++ Control/kitCommand/Help.php
@@ -11,25 +11,24 @@
 
 namespace phpManufaktur\Basic\Control\kitCommand;
 
-use Silex\Application;
+use phpManufaktur\Basic\Control\kitCommand\Basic as kitCommand;
 
-class Help {
+class Help extends kitCommand {
 
     const USERAGENT = 'kitFramework:Basic';
 
-    protected $app = null;
-
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
+    /**
+     * Get the content of the specified help file from Gist
+     *
+     * @param string $info_path to the command.xxx.json file
+     */
     public function getContent($info_path)
     {
         $info = $this->app['utils']->readConfiguration($info_path);
-        if (isset($info['help'][$this->app['locale']]['gist_id'])) {
-            $gist_id = $info['help'][$this->app['locale']]['gist_id'];
-            $gist_link = (isset($info['help'][$this->app['locale']]['link'])) ? $info['help'][$this->app['locale']]['link'] : '';
+        $locale = $this->app['request']->query->get('locale', $this->getCMSlocale());
+        if (isset($info['help'][$locale]['gist_id'])) {
+            $gist_id = $info['help'][$locale]['gist_id'];
+            $gist_link = (isset($info['help'][$locale]['link'])) ? $info['help'][$locale]['link'] : '';
         }
         elseif (isset($info['help']['en']['gist_id'])) {
             $gist_id = $info['help']['en']['gist_id'];
@@ -55,9 +54,10 @@ class Help {
                         $help = array(
                             'command' => $info['command'],
                             'content' => $file['content'],
-                            'link' => $gist_link
+                            'link' => $gist_link,
+                            'help' => FRAMEWORK_URL.'/help/command/help?pid='.$this->getParameterID()
                         );
-                        return $this->app['twig']->render($this->app['utils']->templateFile('@phpManufaktur/Basic/Template', 'kitcommand.help.twig'),
+                        return $this->app['twig']->render($this->app['utils']->templateFile('@phpManufaktur/Basic/Template', 'kitcommand.help.content.twig'),
                             array('help' => $help));
                     }
                 }
@@ -65,7 +65,67 @@ class Help {
         }
         else {
             return $this->app['twig']->render($this->app['utils']->templateFile('@phpManufaktur/Basic/Template', 'kitcommand.help.unavailable.twig'),
-                array('command' => $info['command']));
+                array(
+                    'command' => $info['command'],
+                    'curl_info' => $curl_info
+            ));
         }
     }
-}
+
+    /**
+     * Get the help page for the specified kitCommand from Github Gist
+     *
+     * @param string $command
+     */
+    public function getHelpPage($command)
+    {
+        if (false === ($info_path = $this->getInfoPath($command))) {
+            $this->setMessage('There is no help available for the kitCommand <b>%command%</b>.', array('%command%' => $command));
+            $help = '';
+        }
+        else {
+            $help = $this->getContent($info_path);
+            $info = $this->app['utils']->readConfiguration($info_path);
+        }
+
+        $locale = $this->app['request']->query->get('locale', $this->getCMSlocale());
+
+        return $this->app['twig']->render($this->app['utils']->templateFile('@phpManufaktur/Basic/Template', 'kitcommand.help.twig'),
+            array(
+                'help' => $help,
+                //'command' => $command,
+                'basic' => $this->getBasicSettings(),
+                'command' => array(
+                    'command' => $command,
+                    'general' => array(
+                        'help' => array(
+                            'url' => FRAMEWORK_URL.'/basic/help/help?pid='.$this->getParameterID()
+                        ),
+                        'list' => array(
+                            'url' => FRAMEWORK_URL.'/basic/list?pid='.$this->getParameterID()
+                        ),
+                        'extern' => array(
+                            'url' => FRAMEWORK_URL."/basic/help/$command"
+                        )
+                    ),
+                    'info' => array(
+                        'url' => (isset($info['info'][$locale]['link'])) ? $info['info'][$locale]['link'] :
+                                    ((isset($info['info']['en']['link'])) ? $info['info']['en']['link'] : null)
+                    ),
+                    'wiki' => array(
+                        'url' => (isset($info['wiki'][$locale]['link'])) ? $info['wiki'][$locale]['link'] :
+                                    ((isset($info['wiki']['en']['link'])) ? $info['wiki']['en']['link'] : null)
+                    ),
+                    'issues' => array(
+                        'url' => (isset($info['issues'][$locale]['link'])) ? $info['issues'][$locale]['link'] :
+                                    ((isset($info['issues']['en']['link'])) ? $info['issues']['en']['link'] : null)
+                    ),
+                    'support' => array(
+                        'url' => (isset($info['support'][$locale]['link'])) ? $info['support'][$locale]['link'] :
+                                    ((isset($info['support']['en']['link'])) ? $info['support']['en']['link'] : null)
+                    )
+                ),
+            ));
+    }
+
+ }
