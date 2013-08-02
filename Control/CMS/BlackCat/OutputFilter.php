32e9968eddaf8d582a3509091a7574873e19821f
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-08-02 18:12:13 +0200

    moved output filter from the kitFramework CMS Tool to the Basic extension

---
+++ Control/CMS/BlackCat/OutputFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * kitFramework::Basic
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\Basic\Control\CMS\BlackCat;
+
+use phpManufaktur\Basic\Control\kitCommand\OutputFilter as kitCommandOutputFilter;
+
+// no autoloading at this point !!!
+require_once WB_PATH.'/kit2/extension/phpmanufaktur/phpManufaktur/Basic/Control/kitCommand/OutputFilter.php';
+
+class OutputFilter extends kitCommandOutputFilter
+{
+    // nothing to extend or to change
+}
