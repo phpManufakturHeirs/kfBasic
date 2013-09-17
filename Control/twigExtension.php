6de5ba0719f29211d427adc79caa0751d8f27c1c
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-09-17 03:54:15 +0200

    changed handling for Twig extensions

--- Control/twigExtension.php
+++ Control/twigExtension.php
@@ -14,6 +14,7 @@ namespace phpManufaktur\Basic\Control;
 use Twig_Extension;
 use Twig_SimpleFunction;
 use Silex\Application;
+use phpManufaktur\Basic\Data\Security\Users as frameworkUsers;
 
 require_once MANUFAKTUR_PATH . '/Basic/Control/twigFunction.php';
 
@@ -72,6 +73,7 @@ class twigExtension extends Twig_Extension
         );
     } // getGlobals()
 
+
     /**
      *
      * @see Twig_Extension::getFunctions()
@@ -79,17 +81,154 @@ class twigExtension extends Twig_Extension
     public function getFunctions ()
     {
         return array(
-            new Twig_SimpleFunction('is_authenticated', 'twig_is_authenticated'),
-            new Twig_SimpleFunction('user_display_name', 'twig_user_display_name'),
-            new Twig_SimpleFunction('template_file', 'twig_template_file'),
-            new Twig_SimpleFunction('parse_command', 'twig_parse_command'),
-            new Twig_SimpleFunction('command', 'twig_exec_command'),
-            new Twig_SimpleFunction('recaptcha', 'twig_recaptcha'),
-            new Twig_SimpleFunction('recaptcha_is_active', 'twig_recaptcha_is_active'),
-            new Twig_SimpleFunction('mailhide_is_active', 'twig_mailhide_is_active'),
-            new Twig_SimpleFunction('mailhide', 'twig_mailhide')
+            'isAuthenticated' => new \Twig_Function_Method($this, 'isAuthenticated'),
+            'getUserDisplayName' => new \Twig_Function_Method($this, 'getUserDisplayName'),
+            'template_file' => new \Twig_Function_Method($this, 'template_file'),
+            'getTemplateFile' => new \Twig_Function_Method($this, 'getTemplateFile'),
+            'kitCommandParser' => new \Twig_Function_Method($this, 'kitCommandParser'),
+            'kitCommand' => new \Twig_Function_Method($this, 'kitCommand'),
+            'reCaptcha' => new \Twig_Function_Method($this, 'reCaptcha'),
+            'reCaptchaIsActive' => new \Twig_Function_Method($this, 'reCaptchaIsActive'),
+            'mailHide' => new \Twig_Function_Method($this, 'mailHide'),
+            'mailHideIsActive' => new \Twig_Function_Method($this, 'mailHideIsActive')
         );
-    } // getFunctions()
+    }
+
+
+    /**
+     * Check if the user is authenticated
+     *
+     * @return boolean
+     */
+    function isAuthenticated ()
+    {
+        $token = $this->app['security']->getToken();
+        return !is_null($token);
+    }
+
+    /**
+     * Get the display name of the authenticated user
+     *
+     * @throws Twig_Error
+     * @return string Ambigous string, mixed>
+     */
+    function getUserDisplayName()
+    {
+        try {
+            $token = $this->app['security']->getToken();
+            if (is_null($token))
+                return 'ANONYMOUS';
+            // get user by token
+
+            $user = $token->getUser();
+            // get the user record
+            $frameworkUsers = new frameworkUsers($this->app);
+            if (false === ($user_data = $frameworkUsers->selectUser($user->getUsername()))) {
+                // user not found!
+                return 'ANONYMOUS';
+            }
+            $display_name = (isset($user_data['displayname']) && ! empty($user_data['displayname'])) ? $user_data['displayname'] : $user_data['username'];
+            return $display_name;
+        } catch (\Exception $e) {
+            throw new \Twig_Error($e->getMessage());
+        }
+    }
+
+    /**
+     * Get the template depending on namespace and the framework settings for the template itself
+     *
+     * @deprecated template_file() is deprecated since kfBasic 0.33, use getTemplateFile() instead
+     */
+    function template_file($template_namespace, $template_file, $preferred_template='')
+    {
+        trigger_error('template_file() is deprecated since kfBasic 0.33, use getTemplateFile() instead', E_USER_DEPRECATED);
+        return Utils::templateFile($template_namespace, $template_file, $preferred_template);
+    }
+
+    /**
+     * Get the template depending on namespace and the framework settings for the template itself
+     *
+     * @param string $template_namespace
+     * @param string $template_file
+     * @param string $preferred_template
+     * @return string
+     */
+    function getTemplateFile($template_namespace, $template_file, $preferred_template='')
+    {
+        return $this->app['utils']->templateFile($template_namespace, $template_file, $preferred_template);
+    }
+
+    /**
+     * Parse the content for kitCommands and execute them
+     *
+     * @param Application $app
+     * @param string $content
+     * @return string parsed content
+     */
+    function kitCommandParser($content)
+    {
+        return $this->app['utils']->parseKITcommand($content);
+    }
+
+    /**
+     * Execute a kitCommand with the given parameter
+     *
+     * @param Application $app
+     * @param string $command
+     * @param array $parameter
+     */
+    function kitCommand($command, array $parameter=array())
+    {
+        return $this->app['utils']->execKITcommand($command, $parameter);
+    }
+
+    /**
+     * Return a ReCaptcha dialog if the ReCaptcha service is active
+     *
+     * @param Application $app
+     * @param string theme to use (override global settings)
+     * @param string widget to use for 'custom' theme
+     *
+     * @link https://developers.google.com/recaptcha/docs/customization
+     */
+    function reCaptcha($theme=null, $widget=null)
+    {
+        return $this->app['recaptcha']->getHTML($theme, $widget);
+    }
+
+    /**
+     * Check if the ReCaptcha Service is active or not
+     *
+     * @param Application $app
+     * @return boolean
+     */
+    function reCaptchaIsActive()
+    {
+        return $this->app['recaptcha']->isActive();
+    }
+
+    /**
+     * Return a MailHide link if the service is active. Otherwise, if $mailto is true,
+     * return a complete mailto link. You can set an optional $class for this mailto link
+     *
+     * @param string $email
+     * @param string $title
+     * @param boolean $mailto
+     * @param string $class
+     */
+    public function MailHide($email, $title='', $class='', $mailto=true)
+    {
+        return $this->app['recaptcha']->MailHideGetHTML($email, $title, $class, $mailto);
+    }
+
+    /**
+     * Check if the MailHide Service is active or not
+     *
+     */
+    public function MailHideIsActive()
+    {
+        return $this->app['recaptcha']->MailHideIsActive();
+    }
 
 } // class twigExtension
 
