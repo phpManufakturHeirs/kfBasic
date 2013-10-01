3b347087b122415f37795db523c29f4eb4b707cb
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-10-01 09:14:05 +0200

    Added getPageTitle() Added missing comments

--- Data/CMS/Page.php
+++ Data/CMS/Page.php
@@ -21,6 +21,12 @@ class Page {
     protected $app = null;
     protected $cms = null;
 
+    /**
+     * Constructor
+     *
+     * @param Application $app
+     * @throws \Exception
+     */
     public function __construct(Application $app)
     {
         $this->app = $app;
@@ -36,6 +42,37 @@ class Page {
         }
     }
 
+    /**
+     * Get the page extension with leading dot, by default '.php'
+     *
+     * @throws \Exception
+     * @return string page extension
+     */
+    public function getPageExtension()
+    {
+        return $this->cms->getPageExtension();
+    }
+
+    /**
+     * Get the page directory with leading slash, by default '/pages' or '/page'
+     *
+     * @throws \Exception
+     * @return string page directory
+     */
+    public function getPageDirectory()
+    {
+        $this->cms->getPageDirectory();
+    }
+
+    /**
+     * Get the URL of the given page ID. If arguments 'topic_id' or 'post_id'
+     * the function will return the URL for the given TOPICS or NEWS article
+     *
+     * @param integer $page_id
+     * @param null|array $arguments
+     * @throws \Exception
+     * @return string URL of the page
+     */
     public function getURL($id, $command_parameter=null)
     {
         $parameter = null;
@@ -49,7 +86,37 @@ class Page {
                 $parameter['topic_id'] = $command_parameter['cms']['special']['post_id'];
             }
         }
+        else {
+            $parameter = $command_parameter;
+        }
         return $this->cms->getURL($id, $parameter);
     }
 
+    /**
+     * Get the title of the given page ID. If arguments 'topic_id' or 'post_id'
+     * the function will return the title for the given TOPICS or NEWS article
+     *
+     * @param integer $page_id
+     * @param null|array $arguments
+     * @throws \Exception
+     * @return string title of the page
+     */
+    public function getTitle($page_id, $command_parameter=null)
+    {
+        $parameter = null;
+        if (is_array($command_parameter) && (isset($command_parameter['cms']['special']['topic_id']) ||
+            isset($command_parameter['cms']['special']['post_id']))) {
+            $parameter = array();
+            if (isset($command_parameter['cms']['special']['topic_id'])) {
+                $parameter['topic_id'] = $command_parameter['cms']['special']['topic_id'];
+            }
+            if (isset($command_parameter['cms']['special']['post_id'])) {
+                $parameter['topic_id'] = $command_parameter['cms']['special']['post_id'];
+            }
+        }
+        else {
+            $parameter = $command_parameter;
+        }
+        return $this->cms->getTitle($page_id, $parameter);
+    }
 }
--- Data/CMS/WebsiteBaker/Page.php
+++ Data/CMS/WebsiteBaker/Page.php
@@ -17,11 +17,22 @@ class Page
 {
     protected $app = null;
 
+    /**
+     * Constructor
+     *
+     * @param Application $app
+     */
     public function __construct(Application $app)
     {
         $this->app = $app;
     }
 
+    /**
+     * Get the page extension with leading dot, by default '.php'
+     *
+     * @throws \Exception
+     * @return string page extension
+     */
     public function getPageExtension()
     {
         try {
@@ -32,6 +43,12 @@ class Page
         }
     }
 
+    /**
+     * Get the page directory with leading slash, by default '/pages' or '/page'
+     *
+     * @throws \Exception
+     * @return string page directory
+     */
     public function getPageDirectory()
     {
         try {
@@ -42,6 +59,15 @@ class Page
         }
     }
 
+    /**
+     * Get the URL of the given page ID. If arguments 'topic_id' or 'post_id'
+     * the function will return the URL for the given TOPICS or NEWS article
+     *
+     * @param integer $page_id
+     * @param null|array $arguments
+     * @throws \Exception
+     * @return string URL of the page
+     */
     public function getURL($page_id, $arguments=null)
     {
         try {
@@ -73,6 +99,45 @@ class Page
             $SQL = "SELECT `link` FROM `".CMS_TABLE_PREFIX."pages` WHERE `page_id`='$page_id'";
             $page_link = $this->app['db']->fetchColumn($SQL);
             return CMS_URL. $this->getPageDirectory(). $page_link . $this->getPageExtension();
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Get the title of the given page ID. If arguments 'topic_id' or 'post_id'
+     * the function will return the title for the given TOPICS or NEWS article
+     *
+     * @param integer $page_id
+     * @param null|array $arguments
+     * @throws \Exception
+     * @return string title of the page
+     */
+    public function getTitle($page_id, $arguments=null)
+    {
+        try {
+            if (isset($arguments['topic_id']) && !is_null($arguments['topic_id'])) {
+                // indicate a TOPICS page
+                if (!file_exists(CMS_PATH . '/modules/topics/module_settings.php')) {
+                    throw new \Exception('A TOPIC_ID was submitted, but the TOPICS addon is not installed at the parent CMS!');
+                }
+                // get the title
+                $SQL = "SELECT `title` FROM `".CMS_TABLE_PREFIX."mod_topics` WHERE `topic_id`='".$arguments['topic_id']."'";
+                return $this->app['db']->fetchColumn($SQL);
+            }
+
+            if (isset($arguments['post_id']) && !is_null($arguments['post_id'])) {
+                // indicate a NEWS page
+                if (!file_exists(CMS_PATH. '/modules/news/info.php')) {
+                    throw new \Exception('A POST_ID was submitted, but the NEWS addon is not installed at the parent CMS!');
+                }
+                $SQL = "SELECT `title` FROM `".CMS_TABLE_PREFIX."mod_news_posts` WHERE `post_id`='".$arguments['post_id']."'";
+                return $this->app['db']->fetchColumn($SQL);
+            }
+
+            // regular CMS page
+            $SQL = "SELECT `page_title` FROM `".CMS_TABLE_PREFIX."pages` WHERE `page_id`='$page_id'";
+            return $this->app['db']->fetchColumn($SQL);
         } catch (\Doctrine\DBAL\DBALException $e) {
             throw new \Exception($e);
         }
